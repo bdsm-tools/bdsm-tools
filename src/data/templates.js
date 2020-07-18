403c01ca734cfdaf58dff2902a93a687e2d90c90
Secret Stuff <keepsafemaster@gmail.com>
2020-07-18 15:26:04 +0100

    better layouts, starting with form rendering

---
+++ src/data/templates.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Simple Contract',
+        questions: [
+            {
+                label: 'Question',
+                description: '',
+                imageUrl: null,
+            }
+        ],
+        answerLabels: [
+            '...',
+            'Soft Limit',
+            'Hard Limit',
+        ]
+    }
+]
+
