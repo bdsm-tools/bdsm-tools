72d18a12f8bb5425aea39d07bc441954d6ad19ae
Secret Stuff <keepsafemaster@gmail.com>
2020-08-12 04:10:32 +0100

    updated some loading components and fixed some logic

--- src/scene-negotiation/NegotiationForm.jsx
+++ src/scene-negotiation/NegotiationForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import {Button, Modal} from "antd";
+import {Button, Modal, Popconfirm, Skeleton} from "antd";
 import {JsonForms} from '@jsonforms/react';
 import {vanillaRenderers, vanillaCells} from '@jsonforms/vanilla-renderers';
 import AntdRenderers from '../jsonforms/AntdRenderers';
@@ -10,24 +9,26 @@ import CustomCells from '../jsonforms/CustomCells';
 import api from '../services/scene-negotiation-api';
 import ShareForm from "./ShareForm";
 
-function fetchData({ id, setData, setTemplate }) {
-  return api.getNegotiation(id).then(({ data, template }) => {
-    if (data && template) {
-      // api.getNegotiationType(template)
-      //   .then(setTemplate);
-
-      setData(data);
-    }
-  });
-}
-
 function NegotiationForm(props) {
   const [template, setTemplate] = React.useState(props.template);
   const [data, setData] = React.useState({});
   const [errors, setErrors] = React.useState([]);
   const [submit, setSubmit] = React.useState(false);
   const [readOnly, setReadOnly] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const { search } = props.location;
+  const { push } = props.history;
+
+  const fetchData = (id) => {
+    return api.getNegotiation(id).then(({ data, template }) => {
+      if (data && template) {
+        // api.getNegotiationType(template)
+        //   .then(setTemplate);
+
+        setData(data);
+      }
+    });
+  }
 
   React.useEffect(() => {
     setTemplate(props.template);
@@ -39,12 +40,19 @@ function NegotiationForm(props) {
     const id = params.get('id');
 
     if (id) {
-      fetchData({ id, setData, setTemplate })
-        .then(() => setReadOnly(true));
+      setLoading(true);
+      fetchData(id)
+        .then(() => {
+          setReadOnly(true);
+          setLoading(false);
+        });
     } else if (sourceId) {
-      api.getNegotiation(sourceId).then(source => {
-        setData(source.data);
-      });
+      setLoading(true);
+      fetchData(sourceId)
+        .then(() => {
+          setReadOnly(false);
+          setLoading(false);
+        });
     }
   }, [search]);
 
@@ -58,6 +66,16 @@ function NegotiationForm(props) {
   ];
   if (!template) {
     return null;
+  }
+
+  if (loading) {
+    return (
+      <React.Fragment>
+        <Skeleton active size="large"/>
+        <Skeleton active size="large"/>
+        <Skeleton active size="large"/>
+      </React.Fragment>
+    );
   }
 
   return (
@@ -86,6 +104,29 @@ function NegotiationForm(props) {
         >
           Complete Negotiation
         </Button>
+      }
+      {readOnly &&
+        <Popconfirm
+          title="Are you sure you want to modify? You will not modify this data, only copy it to a new form"
+          placement="top"
+          onConfirm={() => {
+            const params = new URLSearchParams(search);
+            const id = params.get('id');
+
+            push({
+              search: `?source=${id}`,
+            });
+          }}
+        >
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            style={{ marginTop: 20, width: '100%' }}
+          >
+            Modify Negotiation
+          </Button>
+        </Popconfirm>
       }
       <Modal
         visible={submit}
