20b05fdd51f2f66f494ae8ac057703a49453e130
Secret Stuff <keepsafemaster@gmail.com>
2020-08-13 16:01:07 +0100

    effect now runs one on mount

--- src/services/Analytics.jsx
+++ src/services/Analytics.jsx
@@ -4,7 +4,7 @@ import ReactGA from "react-ga";
 export default function Analytics({ location }) {
   React.useEffect(() => {
     ReactGA.initialize('UA-175178769-1');
-  });
+  }, []);
   React.useEffect(() => {
     ReactGA.pageview(location.pathname + location.search);
   }, [location.pathname, location.search]);
