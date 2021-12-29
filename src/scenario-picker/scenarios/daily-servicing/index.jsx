f5bf479a73ec6ae2b0813706282e497a85c48949
Keepsafe Master <54868812+keepsafemaster@users.noreply.github.com>
2021-12-29 06:51:56 +0000

    Develop (#28)
    * upped lots of dependency versions, and added new tool to show scenarios
    
    * npm audit fix
    
    * added more data
    
    * fixed linting
    
    * added my equipment filter
    
    * added some more useful filters
    
    * improved filter layouts
    
    * fixed some deps and linting
    
    * added more scenarios
    
    * minor improvments
    
    * added ide settings

---
+++ src/scenario-picker/scenarios/daily-servicing/index.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+import {H2, H3, H4, Optional, P} from "../../../components/Text";
+import Img from './img.svg';
+
+export default function index() {
+    return (
+        <>
+            <H2>
+                Setup
+            </H2>
+            <P>
+                Lay the straps on the floor and then lay the Slave on top of them with the Slave’s arms by their side
+                and legs together. Make sure their cock and balls are protruding out from their legs (not trapped).
+                Tighten the straps around the Slave’s body (you’ll probably have to keep tightening them throughout
+                the scene); 1 around the feet just above the ankles, 1 around the legs just above the knees, 1 around
+                the thighs just below the cock and balls, 1 around the stomach and hands just above both the waist
+                and wrists, 1 around the arms just above the elbows. If you have more straps, put them where it makes
+                sense (the more straps the more restricting it is).
+            </P>
+            <Img/>
+            <H2>
+                Extra
+            </H2>
+            <P>
+                <ul>
+                    <li>Use a blindfold on the Slave so he doesn’t know what’s coming</li>
+                    <li>Put a butt plug up the Slave’s ass to make him feel more violated</li>
+                    <li>Use a cock sling or similar to make the erection harder</li>
+                    <li>Or start your Slave in a cock cage (it’s best to do this before putting the straps on)</li>
+                    <li>Wear a dominant outfit</li>
+                </ul>
+            </P>
+            <H2>
+                Scene
+            </H2>
+            <H3>
+                Introduction
+            </H3>
+            <P>
+                Once the Slave is set up, leave him like that for 5 minutes or so to build anticipation. Once you feel
+                enough time has passed, walk in, try to make an obvious noise of you coming closer (maybe the sound
+                of heels on a hard floor or ping of a whip or flogger getting closer).
+            </P>
+            <P>
+                Start teasing the Slave, tell him what you are going to use him for and why he’s in this situation.
+                Caress the Slave’s cock to show you have control (talk while you do this, teasing him about his
+                situation and the control you have over him). <Optional>Use a flogger to hit the Slave, make sure
+                to tell the Slave why he is receiving this flogging.</Optional>
+                <ul>
+                    <li>
+                        If the Slave talks at all - cover his mouth with your hand saying he must only use his
+                        mouth to pleasure you and the only reason he isn’t gagged is because you’re going to be
+                        using it.
+                    </li>
+                    <li>
+                        If the Slave struggles - tell him he’s not going anywhere until you get what you are owed (a
+                        sexual servicing)
+                    </li>
+                </ul>
+            </P>
+            <H3>
+                The reason we are here
+            </H3>
+            <P>
+                Once the Slave is warmed up with your touching <Optional>and flogging</Optional>. Proceed to expose your
+                pussy, move it over your Slave’s face close enough so he can smell it (this will work even better if he
+                is blindfolded). Once he has the aroma of your pussy return to touching his cock and balls being a bit
+                more rough this time, continue keeping you pussy hovering close to his face.
+            </P>
+            <H3>
+                The servicing
+            </H3>
+            <P>
+                When you think the Slave has imagined and anticipated enough, sit on his face getting his nose up your
+                pussy, stay there for 5 seconds before allowing him to breathe. Wait for 5 more seconds before repeating
+                (5 seconds on, 5 seconds off), do this over and over to show him how much control you have (enough to
+                control his breathing). <Optional>Touch and squeeze his cock and balls while you do this (make sure you
+                stick to the 5 seconds though)</Optional>. Make sure the Slave knows how much control you have.
+            </P>
+            <P>
+                Command the Slave to stick out his tongue and then lick your pussy as you bring it closer to his face.
+                Make sure he is doing it right and to your liking, direct him to doing it right and if he’s still doing
+                a poor job, stop his breath with your pussy for 5 seconds. <Optional>If that still doesn’t make him do
+                it properly then add some nipple clamps to make him more enthusiastic</Optional>. When you are satisfied
+                (don’t worry you’ll be back later) move your ass hole over his mouth and command him to lick it, stick
+                his tongue inside and give it a good clean (you should spread your ass cheeks to give him room to get
+                all the spots. <Optional>Alternate between your ass being licked and your pussy.</Optional> Now tell
+                your Slave to keep his tongue out as big as it can. Now move your body so the Slave’s tongue licks from
+                your ass hole all the way to your clit (or vice versa). Do this for 30 seconds, if the Slave puts his
+                tongue back in his mouth, start again. If the Slave has nipple clamps on, pull them off and replace them
+                each time he does this.
+            </P>
+            <H3>
+                The human sex toy
+            </H3>
+            <P>
+                The Slave’s cock should be rock hard at this point, now you can use him as a sex toy. Ride his dick in
+                the way you like it, make yourself cum if you like, make him wish he was the one making you cum. Don’t
+                let the Slave cum while you ride him. When riding him do so somewhat aggressively so you pound the
+                Slave’s balls when you bounce up and down. Once you are satisfied with that, dismount telling the Slave
+                what a good sex toy they can be. <Optional>Tease him by licking your juices off his dick making him
+                think you may suck his dick (tell him the only reason you’d put your mouth near his dick if for the
+                taste of pussy). Or go back to sit on his face getting him covered in your juices.</Optional>
+                <ul>
+                    <li>
+                        If the Slave is cumming - dismount immediately, ruin his orgasm and let his cum dribble out of
+                        his cock. Leave for 20 minutes or more and let him lay there, still bound, ready to be used
+                        further. <Optional>Instead of leaving (at all or for as long as 20 minutes) you could return to
+                        making him lick your ass and pussy.</Optional> Then return to continue your pleasure on his
+                        dick.
+                    </li>
+                </ul>
+            </P>
+            <H3>
+                Ending
+            </H3>
+            <P>
+                To finish the scene you have two routes depending on how satisfied you are with your Slave. If the Slave
+                has pleasured you to a satisfactory level choose the “Reward Ending” if not choose the “Punishment
+                Ending”
+            </P>
+            <H4>
+                Reward Ending
+            </H4>
+            <P>
+                The Slave has done a good job serving you despite the initial start and will get to cum. You can do one
+                of a few things:
+                <ol type='A'>
+                    <li>
+                        Give him a hand job going as slow as possible, make him work for it (maybe just keep your hand
+                        steady, let him fuck it). Use a slow stroke to begin with, then once he’s warmed up continue
+                        faster (maybe try a short interval aggressive stroke; stroke very fast just on the tip for 2
+                        seconds, then stop for 2 seconds, repeat). Continue stroking until he cums. <Optional>Lick up
+                        his cum telling him it’s delicious to drive him crazy.</Optional>
+                    </li>
+                    <li>
+                        Mount his cock again, this time ride him more how he likes it. Start off slow and increase speed
+                        (maybe hover above his dick and make him fuck you). Choose whether he cums inside of you or not
+                        (hey, maybe you can cum again before he does). <Optional>Give his dick one suck at the end to
+                        drive him crazy.</Optional>
+                    </li>
+                    <li>
+                        Use a milking machine to stroke his cock for you. Make sure to use a lot of lube and start off
+                        slow. You could get back on the Slave’s face and make him lick your ass and pussy while you
+                        control the speed of the milking machine. See if the faster strokes makes him a better pussy
+                        licker or a worse one. Tell him you will match the speed of the milker to the performance of his
+                        licking. <Optional>Once he has cum, leave the milking machine running to torture him some
+                        more.</Optional>
+                    </li>
+                </ol>
+            </P>
+            <H4>
+                Punishment Ending
+            </H4>
+            <P>
+                The Slave has not serviced you well and is not allowed to cum, instead he must lay there and think about
+                his poor performance. <Optional>But not before another flogging, while hitting, tell him what a poor job
+                he’s done and that he’ll need to improve next time.</Optional> Leave him tied in the same position for
+                at least 30 minutes while you continue with other things in another room. If you need to be in the
+                current room, untie him and move him to another room where you will cuff his hands behind his back
+                around something that will prevent him from leaving that space. Leave him for 30 minutes while you do
+                other things before ending the scene.
+            </P>
+        </>
+    )
+}
