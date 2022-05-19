5aea98ff0f751d0967f2f703e6b7084fd3474bb6
Secret Stuff <keepsafemaster@gmail.com>
2022-05-19 23:39:14 +0100

    versions

--- api/scene-negotiation/deploy.sh
+++ api/scene-negotiation/deploy.sh
@@ -1,7 +1,7 @@
 gcloud config set project bdsm-tools &&
 gcloud functions deploy scene-negotiation-test \
   --entry-point doProcess \
-  --runtime nodejs10 \
+  --runtime nodejs14 \
   --trigger-http \
   --allow-unauthenticated \
   --memory 512MB \
